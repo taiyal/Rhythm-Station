b2314dd2adcd0e3192193e696c02c13de845836f
Colby Klein <shakesoda@gmail.com>
2010-09-02 02:21:05 -0700

    Update TimingData, add Lua to VC project [AJ]

--- RStation/TimingData.h
+++ RStation/TimingData.h
@@ -7,6 +7,7 @@ enum NoteType {
 	NOTE_TAP,
 	NOTE_HOLD,
 	NOTE_ROLL,
+	NOTE_TAIL,
 	NOTE_MINE
 };
 
@@ -17,22 +18,47 @@ enum NoteAttribute {
 
 struct Note
 {
+	// Generic stuff.
 	NoteType type;
-	int column;
-	float width;
-	vec3 position;
 	std::vector<NoteAttribute> flags;
+
+	// For games which use columns (StepMania-esque)
+	int column;
+	int width; // cover multiple columns
+
+	// For mouse-based games which use coords (osu!-esque)
+	float scale;
+	vec3 position;
 };
 
-struct TimingData
+struct TimeSignature
 {
-	std::vector<Note> note_row; // notes on this row
-	int time; // time in milliseconds.
+	int numerator;
+	int denominator;
+};
+
+struct NoteRegion
+{
+	int combo_rate; // aka tickcount
+	float bpm; // For display purposes, not timing.
+	TimeSignature time_sig;
+};
+
+struct NoteRow
+{
+	std::vector<Note> notes; // Notes on this row
+	int time; // Time in milliseconds.
 	/*
 	 * Note: This can be zero. Notes will always arrive on time, regardless
 	 * of the scrolling speed.
 	 */
 	float scroll_speed;
+};
+
+struct TimingData
+{
+	std::vector<NoteRow> note_rows; // all the note rows in this data
+	std::vector<NoteRegion> note_regions;
 };
 
 #endif
