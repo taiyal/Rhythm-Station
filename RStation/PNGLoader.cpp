1fbc42d8bf5c850798a0a8db0c78aa8c520942dc
corec <corecrystal@gmail.com>
2010-09-13 18:04:45 +0300

    libpng-1.4 support

--- RStation/PNGLoader.cpp
+++ RStation/PNGLoader.cpp
@@ -59,7 +59,7 @@ void PNGLoader::Load(std::string _path)
 		png_set_palette_to_rgb(png_ptr);
 
 	if(format == PNG_COLOR_TYPE_GRAY && bitDepth < 8)
-		png_set_gray_1_2_4_to_8(png_ptr);
+		png_set_expand_gray_1_2_4_to_8(png_ptr);
 
 	if(png_get_valid(png_ptr, info_ptr, PNG_INFO_tRNS))
 		png_set_tRNS_to_alpha(png_ptr);
